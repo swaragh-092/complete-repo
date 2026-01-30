aa8bccbb46d1e9a4522f93bd251c3edf8641c69b
Spidy092 <Swaragh092@gmail.com>
2026-01-30 10:37:56 +0530

    docker fix

--- pms/config/config.js
+++ pms/config/config.js
@@ -1,23 +1,23 @@
 const domain = {
-    superAdmin: 'http://super_administrator:8089/',
-    auth: 'http://auth_service:8081/',
+    superAdmin: 'http://super-administrator:8089/',
+    auth: 'http://auth-service:4000/',
 }
 // const domain = {
 //     superAdmin: 'http://localhost:2500',
-//     auth: 'http://auth_service:8081',
+//     auth: 'http://auth-service:4000',
 // }
 
 const databaseDetails = {
-    "default-shared" : {
-        name: 'pms',
-        user: 'postgres',
-        password: '1234',
-        host: 'postgres_db',
-        port: 5432,
+    "default-shared": {
+        name: process.env.DB_NAME || 'pms',
+        user: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || '1234',
+        host: process.env.DB_HOST || 'postgres-auth',
+        port: process.env.DB_PORT || 5432,
         type: 'postgres',
         zone: '+05:30'
     },
-    "single-amd-tenant" : {
+    "single-amd-tenant": {
         name: 'pms_amd',
         user: 'postgres',
         password: '1234',
@@ -30,6 +30,6 @@ const databaseDetails = {
 
 module.exports = {
     domain,
-    moduleCode : "pms_mod",
+    moduleCode: "pms_mod",
     databaseDetails
 };
